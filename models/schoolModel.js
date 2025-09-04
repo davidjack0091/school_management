8b5096c08996d6c3042270565b28740a6c7902dc
davidjack0091 <davidjack0091@outlook.com>
2025-09-04 08:51:27 +0530

    Create schoolModel.js

---
+++ models/schoolModel.js
@@ -0,0 +1,29 @@
+const Sequelize = require('sequelize')
+const sequelize = require('../config/database')
+
+const School = sequelize.define('School', {
+    id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true,
+    },
+    name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+    },
+    address: {
+        type: Sequelize.STRING,
+        allowNull: false,
+    },
+    latitude: {
+        type: Sequelize.FLOAT,
+        allowNull: false,
+    },
+    longitude: {
+        type: Sequelize.FLOAT,
+        allowNull: false,
+    },
+})
+
+module.exports = School
